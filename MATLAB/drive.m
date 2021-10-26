f4a26250785fd1163a66b5cd4d9a975dbdb99fe9
TheseusDeus <46569868+TheseusDeus@users.noreply.github.com>
2021-10-26 13:31:10 +0200

    script drive mod
    fixed pdf error and added script

---
+++ MATLAB/drive.m
@@ -0,0 +1,12 @@
+clear all; clc;
+
+pns = pnstruct('drive_PDF');
+
+dyn.m0 = {'pStartRoute', 1};
+%dyn.m0 = {'pStartRoute', 1, 'pstop',0, 'pLastStop',0};
+dyn.ft = {'allothers', 1}; 
+pni = initialdynamics(pns, dyn);
+sim = gpensim(pni);
+
+prncolormap(sim);
+prnfinalcolors(sim); 
