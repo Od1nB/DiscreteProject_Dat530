6a936c0a2cc21488b65ba25aefb466e8031f1bae
BrynjarUlriksen <bs.ulriksen@stud.uis.no>
2021-10-29 13:10:11 +0200

    connector and main changes

--- MATLAB/module_5_Cleanup/MOD_cleanup_PRE.m
+++ MATLAB/module_5_Cleanup/MOD_cleanup_PRE.m
@@ -1,10 +1,10 @@
-function [fire, transition] = COMMON_PRE(transition)
+function [fire, transition] = MOD_cleanup_PRE(transition)
 
-if (strcmp(transition.name, "tWait")) 
+if (strcmp(transition.name, 'tWait')) 
     fire =1;
-elseif (strcmp(transition.name,"tLobby"))
+elseif (strcmp(transition.name,'tLobby'))
     fire =1;
 else
-    error("transition name does not match")
+    error('transition name does not match')
 end
     
