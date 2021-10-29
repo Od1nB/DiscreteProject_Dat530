93505e5566c8ad67a1280b2dfb2161008130d96d
BrynjarUlriksen <bs.ulriksen@stud.uis.no>
2021-10-29 12:04:03 +0200

    Add connector and update name in draw file

--- MATLAB/module_0_Connector/connector_pdf.m
+++ MATLAB/module_0_Connector/connector_pdf.m
@@ -7,7 +7,7 @@
 function[png] = connector_pdf()
 png.PN_name='main';
 png.set_of_Ps = {};
-png.set_of_Ts = {'tCONNECT1','tCONNECT2','tCONNECT3'};
-png.set_of_As = {'pLOBBY','tCONNECT1',1,'tCONNECT1','pCHECK_IN',1, ...
+png.set_of_Ts = {'tCONNECT1','tCONNECT2','tCONNECT3','tCONNECT4','tCONNECT5'};
+png.set_of_As = {'pAdministration','tCONNECT1',1,'tCONNECT1','pWait',1, ...
     'pCHECK_OUT','tCONNECT2',1,'tCONNECT2','pSEC_IN',1, ...
     'pSEC_OUT','tCONNECT3',1,'tCONNECT3','pPASS_IN',1,};
