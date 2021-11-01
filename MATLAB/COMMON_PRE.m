21a4c430574b1af17ed2d6068871868e52ce94f9
BrynjarUlriksen <brynjar.steinbakk@gmail.com>
2021-11-01 11:15:54 +0100

    Common_pre runs

--- MATLAB/COMMON_PRE.m
+++ MATLAB/COMMON_PRE.m
@@ -1,22 +1,27 @@
 function[fire, transition] = COMMON_PRE(transition)
+% function [fire, trans] = COMMON_PRE(trans)
 global global_info;
 
 %pWait = ntokens('pWait'); %check how many items tokens in pWait if
 %neccesary
 
+%Module 2 preparation 
+
 %Check if can fire
 if(strcmp(transition.name, 'tPick_up_bus_from_parking'))
     transition.new_color = 'bus';
     fire = 1;
-end
-%Check if can fire
-if(strcmp(transition.name, 'tPick_up_car'))
-    transition.new_color = 'vehicle';
-    fire = 1;
-end
 
 %Check if can fire
-if(strcmp(transition.name, 'tDriveToBusStopG'))
+elseif(strcmp(transition.name, 'tPick_up_car'))
+    transition.new_color = 'vehicle';
+    fire = 1;
+
+
+%module 3 routes
+
+%Check if can fire
+elseif(strcmp(transition.name, 'tDriveToBusStopBus'))
     tokID = tokenAny('pRoutes',1);
     colors = get_color('pRoutes',tokID);
     if(strcmp(colors{1},'bus' )) %check if correct color
@@ -24,10 +29,10 @@ if(strcmp(transition.name, 'tDriveToBusStopG'))
     else
         fire = 0;
     end
-end
+
 
 %Check if can fire
-if(strcmp(transition.name, 'tDriveToBusStopR'))
+elseif(strcmp(transition.name, 'tDriveToBusStopCar'))
     tokID = tokenAny('pRoutes',1);
     colors = get_color('pRoutes',tokID);
     if(strcmp(colors{1},'vehicle' )) %check if correct color
@@ -35,5 +40,38 @@ if(strcmp(transition.name, 'tDriveToBusStopR'))
     else
         fire = 0;
     end
+
+
+%Check if can fire
+elseif(strcmp(transition.name, 'tTakeBusToParking'))
+    tokID = tokenAny('pLastStop',1);
+    colors = get_color('pLastStop',tokID);
+    %if(strcmp(colors{1},'bus_done' )) %check if correct color
+    fire = 1;
+    %else
+    %    fire = 0;
+    %end
+
+
+%Check if can fire
+elseif(strcmp(transition.name, 'tChangeDriver'))
+    tokID = tokenAny('pLastStop',1);
+    colors = get_color('pLastStop',tokID);
+    %if(strcmp(colors{1},'switch' )) %check if correct color
+    fire = 1;
+    %else
+    %    fire = 0;
+    %end
+
+
+%Check if can fire
+elseif(strcmp(transition.name, 'tTakeCarToParking'))
+   transition.new_color = 'vehicle_done';
+   transition.override = 1;
+   fire = 1;
+else
+    fire = 1;
 end
+
+%module 5 cleanup
 
